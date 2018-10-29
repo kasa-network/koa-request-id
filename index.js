ab8e2f8a8a601028fb27472ddfc957b7e25128d3
Byungjin Park <posquit0.bj@gmail.com>
2018-10-29 15:34:26 +0900

    Implement reqId middleware

---
+++ index.js
@@ -0,0 +1,6 @@
+'use  strict';
+
+const middleware = require('./lib/middleware');
+
+
+module.exports = middleware;
---
+++ test/middleware.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const requestId = require('../');
+
+
+// describe('request-id', () => {
+//   let middleware;
+
+//   beforeEach(() => {
+//     middleware = requestId();
+//   });
+
+//   describe('something', () => {
+//     it.skip('should return', () => {
+//     });
+//   });
+// });
