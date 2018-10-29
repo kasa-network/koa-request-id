60f178dbf5e666a6e5f962ee97dbc451937a05ea
Byungjin Park <posquit0.bj@gmail.com>
2018-10-29 15:56:55 +0900

    Add basic test cases

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -10,9 +10,15 @@ describe('request-id', () => {
     middleware = requestId();
   });
 
-  describe('something', () => {
-    it('should success', () => {
-      return middleware;
+  it('should be defined as function', () => {
+    expect(requestId).toBeDefined();
+    expect(requestId).toBeFunction();
+  });
+
+  describe('middleware', () => {
+    it('should be defined as koa middleware function', () => {
+      expect(middleware).toBeDefined();
+      expect(middleware).toBeFunction();
     });
   });
 });
